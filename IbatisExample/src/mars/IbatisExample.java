335c19b3d5517a8e23b136c5d18a7f1a614a7913
mars.zhou <mars.zhou@dianping.com>
2015-12-18 12:04:44 +0800

    modify gitignore files

--- IbatisExample/src/mars/IbatisExample.java
+++ IbatisExample/src/mars/IbatisExample.java
@@ -3,43 +3,68 @@ package mars;
 import java.io.IOException;
 import java.io.Reader;
 import java.sql.SQLException;
-import java.util.ArrayList;  
-import java.util.List;  
-  
-import com.ibatis.common.resources.Resources;  
-import com.ibatis.sqlmap.client.SqlMapClient;  
-import com.ibatis.sqlmap.client.SqlMapClientBuilder;  
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
 public class IbatisExample {
 
-	public static void main(String[] args) throws IOException, SQLException {
-		// TODO Auto-generated method stub
-		String resource = "mars/SqlMapConfig.xml";
-		Reader reader = Resources.getResourceAsReader(resource);
-		SqlMapClient sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader);
-		List<User> list = new ArrayList<User>();
-		list = sqlMap.queryForList("getAllUsers", "EMPLOYEE");
+    public static void main(String[] args) throws IOException, SQLException {
+        // TODO Auto-generated method stub
+        String resource = "mars/SqlMapConfig.xml";
+        Reader reader = Resources.getResourceAsReader(resource);
+        SqlMapClient sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader);
 
-		int s = list.size();
-		System.out.println("Selected " + s + " records.");
+        List<User> list = new ArrayList<User>();
+        // User user = (User) sqlMap.queryForObject("getUserById", 1);
+        // System.out.println("Selected " + user.getUserid() +
+        // user.getUsername()
+        // + user.getGroupname());
+        //
+        // list = sqlMap.queryForList("getAllUsers", "dianing");
+        // int s = list.size();
+        // System.out.println("Selected " + s + " records.");
+        //
+        // System.out.println("USERID\tUSERNAME\tPASSWORD\tGROUP");
+        //
+        // for (int i = 0; i < s; i++) {
+        // System.out.print(list.get(i).getUserid() + "\t");
+        // System.out.print(list.get(i).getUsername() + "\t\t");
+        // System.out.print(list.get(i).getPassword() + "\t\t");
+        // System.out.println(list.get(i).getGroupname());
+        // }
 
-		System.out.println("USERID\tUSERNAME\tPASSWORD\tGROUP");
+        // List<Integer> ids = sqlMap.queryForList("getUserIdValues", null);
+        // s = ids.size();
+        // for (int id : ids) {
+        // System.out.println(id);
+        // }
 
-		for (int i = 0; i < s; i++) {
-			System.out.print(list.get(i).getUserid() + "\t");
-			System.out.print(list.get(i).getUsername() + "\t\t");
-			System.out.print(list.get(i).getPassword() + "\t\t");
-			System.out.println(list.get(i).getGroupname());
-		}
-		
-		System.out.print("");
-		
-		User _user = new User();
-		_user = (User) sqlMap.queryForObject("getUserById", 5);
-		System.out.println("USERID\tUSERNAME\tPASSWORD\tGROUP");
-		System.out.print(_user.getUserid() + "\t");
-		System.out.print(_user.getUsername() + "\t\t");
-		System.out.print(_user.getPassword() + "\t\t");
-		System.out.println(_user.getGroupname());
-	}
+        // sqlMap.update("updateGroupName", new String("dianping"));
+        // list = sqlMap.queryForList("getAllUsers", "dianping");
+        // for (User _user : list) {
+        // System.out.print(_user.getUserid() + "\t");
+        // System.out.print(_user.getUsername() + "\t\t");
+        // System.out.print(_user.getPassword() + "\t\t");
+        // System.out.println(_user.getGroupname());
+        // }
+
+        User newUser = new User();
+        newUser.setUserid(10);
+        newUser.setUsername("mars.zhou");
+        newUser.setPassword("root");
+        newUser.setGroupname("poi");
+        sqlMap.insert("insertIntoUsers", newUser);
+        list = sqlMap.queryForList("getAll");
+        for (User user : list) {
+            System.out.print(user.getUserid() + "\t");
+            System.out.print(user.getUsername() + "\t\t");
+            System.out.print(user.getPassword() + "\t\t");
+            System.out.println(user.getGroupname());
+        }
+    }
 
 }
---
+++ marszhou-httpServlet/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@ page pageEncoding="UTF-8" %>
+<html>
+<body>
+<h2>又是 Hello World!</h2>
+</body>
+</html>
