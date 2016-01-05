5b16e2f469be620abed60b1496491f4d07514e45
mars.zhou <mars.zhou@dianping.com>
2016-01-05 14:32:57 +0800

    add aspect

--- smart-framework/src/main/java/org/smart4j/annotation/Aspect.java
+++ smart-framework/src/main/java/org/smart4j/annotation/Aspect.java
@@ -1,7 +1,12 @@
 package org.smart4j.annotation;
 
+import java.lang.annotation.*;
+
 /**
  * Created by marszhou on 15/12/21.
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Aspect {
+    Class<? extends Annotation> value();
 }
--- smart-framework/src/main/java/org/smart4j/proxy/Proxy.java
+++ smart-framework/src/main/java/org/smart4j/proxy/Proxy.java
@@ -4,4 +4,5 @@ package org.smart4j.proxy;
  * Created by marszhou on 15/12/21.
  */
 public interface Proxy {
+    Object doProxy(ProxyChain proxyChain) throws Throwable;
 }
