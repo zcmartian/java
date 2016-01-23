aeba141e383545432e8519e2ce3a495af367f84c
mars.zhou <mars.zhou@dianping.com>
2016-01-23 20:27:01 +0800

    hello world

---
+++ servlet-demo/src/main/java/HelloWorld.java
@@ -0,0 +1,31 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by marszhou on 16/1/23.
+ */
+public class HelloWorld extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        message = "Hello world";
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.print("<h1>" + message + "</h1");
+    }
+
+    @Override
+    public void destroy(){
+        //do nothing;
+    }
+}
