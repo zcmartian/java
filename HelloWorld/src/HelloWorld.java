834190b15dfddbab894f0a5176b68c1d8c3f5f54
zcmartian <zcmit@126.com>
2016-11-02 23:52:00 +0800

    add arraydeque & serializable

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.ArrayDeque;
+
 public class HelloWorld {
 
     /**
@@ -6,6 +8,16 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("Hello world!");
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        int result = -1 & 4;
+        System.out.println(deque.size());
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addLast(3);
+        deque.addLast(4);
+        deque.pollFirst();
+        deque.pollLast();
+        System.out.println(deque.toString());
     }
 
 }
