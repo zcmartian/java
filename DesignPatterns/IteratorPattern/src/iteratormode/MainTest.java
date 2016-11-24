c0333de6825e9d7e9df9754928476e38faf5ee62
zcmartian <zcmit@126.com>
2016-11-24 13:49:46 +0800

    add compose

---
+++ DesignPatterns/IteratorPattern/src/iteratormode/MainTest.java
@@ -0,0 +1,10 @@
+package iteratormode;
+
+public class MainTest {
+    public static void main(String[] args) {
+
+        Waitress mWaitress = new Waitress();
+
+        mWaitress.printMenu();
+    }
+}
---
+++ DesignPatterns/IteratorPattern/src/iteratormode/iterator/Iterator.java
@@ -0,0 +1,9 @@
+package iteratormode.iterator;
+
+public interface Iterator {
+
+    public boolean hasNext();
+
+    public Object next();
+
+}
