4c12726553023c228fc2abd0df65cf3c348f5f04
zcmartian <zcmit@126.com>
2016-11-26 19:58:11 +0800

    add decorator

--- DesignPatterns/DecoratorPattern/src/coffeebar/CoffeeBar.java
+++ DesignPatterns/DecoratorPattern/src/coffeebar/CoffeeBar.java
@@ -4,6 +4,7 @@ import coffeebar.coffee.Decaf;
 import coffeebar.coffee.LongBlack;
 import coffeebar.decorator.Chocolate;
 import coffeebar.decorator.Milk;
+import coffeebar.decorator.Soy;
 
 public class CoffeeBar {
 
@@ -17,7 +18,7 @@ public class CoffeeBar {
         System.out.println("****************");
         order = new LongBlack();
         order = new Milk(order);
-        order = new Chocolate(order);
+        order = new Soy(order);
         order = new Chocolate(order);
         System.out.println("order2 price:" + order.cost());
         System.out.println("order2 desc:" + order.getDescription());
