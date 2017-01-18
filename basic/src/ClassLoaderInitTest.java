beb7f1c9b0edb43c2aa4a129274f87a9b6e2ee5e
mars.zhou <mars.zhou@dianping.com>
2017-01-18 17:59:04 +0800

    add classloader

---
+++ basic/src/ClassLoaderInitTest.java
@@ -0,0 +1,23 @@
+/**
+ * Created by mars on 2017/1/18.
+ */
+
+class CL {
+
+    static {
+        System.out.println("static block in CL");
+    }
+}
+
+public class ClassLoaderInitTest {
+
+    public static void main(String[] args) throws Exception {
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+        Class<?> clazz = loader.loadClass("CL");
+        // loadClass方法加载一个类，并不是对类的主动使用，不会导致类的初始化
+
+        System.out.println("----------------");
+        clazz = Class.forName("CL");
+    }
+
+}
