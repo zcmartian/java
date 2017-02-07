41cdb4d195b42618f0c25e9accef0162fd00d3b1
mars.zhou <mars.zhou@dianping.com>
2017-02-07 15:33:25 +0800

    add bout

---
+++ basic/src/Person.java
@@ -0,0 +1,82 @@
+import java.io.Serializable;
+
+/**
+ * Created by mars on 2017/2/7.
+ */
+public class Person implements Serializable {
+    private static final long serialVersionUID = -9102017020286042305L;
+
+    private String name; // 姓名
+    private int age; // 年龄
+    private Car car; // 座驾
+
+    public Person(String name, int age, Car car) {
+        this.name = name;
+        this.age = age;
+        this.car = car;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", age=" + age + ", car=" + car + "]";
+    }
+
+}
+
+class Car implements Serializable {
+    private static final long serialVersionUID = -5713945027627603702L;
+
+    private String brand; // 品牌
+    private int maxSpeed; // 最高时速
+
+    public Car(String brand, int maxSpeed) {
+        this.brand = brand;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "Car [brand=" + brand + ", maxSpeed=" + maxSpeed + "]";
+    }
+
+}
