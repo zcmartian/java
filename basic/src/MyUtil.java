90bc76f1f12e915a9c38f86f6e687e37681f5b02
mars.zhou <mars.zhou@dianping.com>
2017-02-08 17:24:46 +0800

    add more cases

--- basic/src/MyUtil.java
+++ basic/src/MyUtil.java
@@ -4,6 +4,8 @@
  */
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class MyUtil {
 
@@ -23,6 +25,33 @@ public class MyUtil {
 
         // 说明：调用ByteArrayInputStream或ByteArrayOutputStream对象的close方法没有任何意义
         // 这两个基于内存的流只要垃圾回收器清理对象就能够释放资源，这一点不同于对外部资源（如文件流）的释放
+    }
+
+    public static void fileCopy(String source, String target) throws IOException {
+        try (InputStream in = new FileInputStream(source)) {
+            try (OutputStream out = new FileOutputStream(target)) {
+                byte[] buffer = new byte[4096];
+                int bytesToRead;
+                while((bytesToRead = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, bytesToRead);
+                }
+            }
+        }
+    }
+
+    public static void fileCopyNIO(String source, String target) throws IOException {
+        try (FileInputStream in = new FileInputStream(source)) {
+            try (FileOutputStream out = new FileOutputStream(target)) {
+                FileChannel inChannel = in.getChannel();
+                FileChannel outChannel = out.getChannel();
+                ByteBuffer buffer = ByteBuffer.allocate(4096);
+                while(inChannel.read(buffer) != -1) {
+                    buffer.flip();
+                    outChannel.write(buffer);
+                    buffer.clear();
+                }
+            }
+        }
     }
 }
 
