2e5a88fd01fda23b19ecf3679ec158320841e530
mars.zhou <mars.zhou@dianping.com>
2017-03-01 21:27:28 +0800

    add reference

---
+++ basic/src/Reference.java
@@ -0,0 +1,38 @@
+/**
+ * Created by mars on 2017/3/1.
+ */
+public class Reference {
+    private int value;
+
+    public Reference(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Reference{" +
+                "value=" + value +
+                '}';
+    }
+
+    public static void main(String ... args) {
+        Reference reference = new Reference(100);
+
+        System.out.println(reference);
+        change(reference);
+        System.out.println(reference);
+    }
+
+    private static void change(Reference ref) {
+//        ref.setValue(200);
+        ref = new Reference(200);
+    }
+}
