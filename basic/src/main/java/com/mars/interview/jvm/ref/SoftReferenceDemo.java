7ffcadf30605eaa499a37de3aaf22a2cbb2e8ec1
zcmartian <zcmit@126.com>
2019-04-20 18:04:44 +0800

    add softreference

---
+++ basic/src/main/java/com/mars/interview/jvm/ref/SoftReferenceDemo.java
@@ -0,0 +1,24 @@
+package com.mars.interview.jvm.ref;
+
+import java.lang.ref.SoftReference;
+
+public class SoftReferenceDemo {
+    public static void main(String[] args) {
+        Object o1 = new Object();
+        SoftReference<Object> softReference = new SoftReference<>(o1);
+        System.out.println(o1);
+        System.out.println(softReference.get());
+
+        o1 = null;
+        System.gc();
+        //-Xms5m -Xmx5m -XX:+PrintGCDetails
+        try {
+            byte[] bytes = new byte[30*1024*1024];
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(o1);
+            System.out.println(softReference.get());
+        }
+    }
+}
