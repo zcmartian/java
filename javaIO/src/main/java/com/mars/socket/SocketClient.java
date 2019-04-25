8dd015da14a0c4996ae31cfde2f2c2eba5653703
zhouchao3 <zhouchao3@xiaomi.com>
2019-04-25 16:44:32 +0800

    add socket demos 2,3

--- javaIO/src/main/java/com/mars/socket/SocketClient.java
+++ javaIO/src/main/java/com/mars/socket/SocketClient.java
@@ -10,15 +10,15 @@ public class SocketClient {
         try {
             /** 创建Socket*/
             // 创建一个流套接字并将其连接到指定 IP 地址的指定端口号(本处是本机)
-            Socket socket =new Socket("127.0.0.1",2013);
+            Socket socket = new Socket("127.0.0.1", 2013);
             // 60s超时
             socket.setSoTimeout(60000);
 
             /** 发送客户端准备传输的信息 */
             // 由Socket对象得到输出流，并构造PrintWriter对象
-            PrintWriter printWriter =new PrintWriter(socket.getOutputStream(),true);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
             // 将输入读入的字符串输出到Server
-            BufferedReader sysBuff =new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader sysBuff = new BufferedReader(new InputStreamReader(System.in));
             printWriter.println(sysBuff.readLine());
             // 刷新输出流，使Server马上收到该字符串
             printWriter.flush();
@@ -26,7 +26,7 @@ public class SocketClient {
             /** 用于获取服务端传输来的信息 */
             // 由Socket对象得到输入流，并构造相应的BufferedReader对象
             BufferedReader bufferedReader =
-                new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    new BufferedReader(new InputStreamReader(socket.getInputStream()));
             // 输入读入一字符串
             String result = bufferedReader.readLine();
             System.out.println("Server say : " + result);
@@ -35,7 +35,7 @@ public class SocketClient {
             printWriter.close();
             bufferedReader.close();
             socket.close();
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Exception:" + e);
         }
     }
--- javaIO/src/main/java/com/mars/socket/SocketServer.java
+++ javaIO/src/main/java/com/mars/socket/SocketServer.java
@@ -1,4 +1,5 @@
 package com.mars.socket;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -10,7 +11,7 @@ public class SocketServer {
         try {
             /** 创建ServerSocket*/
             // 创建一个ServerSocket在端口2013监听客户请求
-            ServerSocket serverSocket =new ServerSocket(2013);
+            ServerSocket serverSocket = new ServerSocket(2013);
             while (true) {
                 // 侦听并接受到此Socket的连接,请求到来则产生一个Socket对象，并继续执行
                 Socket socket = serverSocket.accept();
@@ -18,14 +19,14 @@ public class SocketServer {
                 /** 获取客户端传来的信息 */
                 // 由Socket对象得到输入流，并构造相应的BufferedReader对象
                 BufferedReader bufferedReader =
-                    new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 // 获取从客户端读入的字符串
                 String result = bufferedReader.readLine();
                 System.out.println("Client say : " + result);
 
                 /** 发送服务端准备传输的 */
                 // 由Socket对象得到输出流，并构造PrintWriter对象
-                PrintWriter printWriter =new PrintWriter(socket.getOutputStream());
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
                 printWriter.print("hello Client, I am Server!");
                 printWriter.flush();
 
@@ -34,9 +35,9 @@ public class SocketServer {
                 bufferedReader.close();
                 socket.close();
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Exception:" + e);
-        }finally{
+        } finally {
             //serverSocket.close();
         }
     }
