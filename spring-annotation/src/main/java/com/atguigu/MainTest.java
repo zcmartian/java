0c5658a6344900f73c0675d03d418f304def571c
zhouchao3 <zhouchao3@xiaomi.com>
2019-04-28 11:06:22 +0800

    add spring-annotation

---
+++ spring-annotation/src/main/java/com/atguigu/MainTest.java
@@ -0,0 +1,28 @@
+package com.atguigu;
+
+import com.atguigu.bean.Person;
+import com.atguigu.config.MainConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainTest {
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        Person bean = (Person) applicationContext.getBean("person");
+        System.out.println(bean);
+
+        applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
+        bean = applicationContext.getBean(Person.class);
+        System.out.println(bean);
+
+        String[] namesForType = applicationContext.getBeanNamesForType(Person.class);
+        for (String name : namesForType) {
+            System.out.println(name);
+        }
+
+    }
+
+}
