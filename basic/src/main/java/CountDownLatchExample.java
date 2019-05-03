cb93fac69a07ba0f077ee54a16c8fbdb01d3a91a
zcmartian <zcmit@126.com>
2019-05-03 20:42:56 +0800

    modify coundownlatch

--- basic/src/main/java/CountDownLatchExample.java
+++ basic/src/main/java/CountDownLatchExample.java
@@ -15,7 +15,31 @@ public class CountDownLatchExample {
     }
 
 }
+class Decrementer implements Runnable {
 
+    CountDownLatch latch = null;
+
+    public Decrementer(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+            this.latch.countDown();
+
+            Thread.sleep(1000);
+            this.latch.countDown();
+
+            Thread.sleep(1000);
+            this.latch.countDown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
 class Waiter implements Runnable {
 
     CountDownLatch latch = null;
@@ -35,5 +59,4 @@ class Waiter implements Runnable {
 
         System.out.println("Waiter Released");
     }
-
 }
