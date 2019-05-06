7af3c333c3efaa5350d0f1f1ad86e5461c148746
zcmartian <zcmit@126.com>
2019-05-06 14:35:00 +0800

    rename

---
+++ concurrent/src/main/java/com/mars/concurrency/example/publish/Escape.java
@@ -0,0 +1,28 @@
+package com.mars.concurrency.example.publish;
+
+import com.mars.concurrency.annoations.NotRecommend;
+import com.mars.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@NotThreadSafe
+@NotRecommend
+public class Escape {
+
+    private int thisCanBeEscape = 0;
+
+    public Escape() {
+        new InnerClass();
+    }
+
+    public static void main(String[] args) {
+        new Escape();
+    }
+
+    private class InnerClass {
+
+        public InnerClass() {
+            log.info("{}", Escape.this.thisCanBeEscape);
+        }
+    }
+}
