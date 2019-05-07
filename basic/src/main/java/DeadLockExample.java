7c5b6c7754224c4bdecb588c9b2fe4ee856773a6
zhouchao3 <zhouchao3@xiaomi.com>
2019-05-07 15:55:14 +0800

    rm some code

--- basic/src/main/java/DeadLockExample.java
+++ basic/src/main/java/DeadLockExample.java
@@ -1,43 +1,36 @@
 public class DeadLockExample {
 
     public static void main(String[] args) throws InterruptedException {
-        // TODO Auto-generated method stub
         final Object o1 = new Object();
         final Object o2 = new Object();
 
-        Thread thread1 = new Thread() {
-            public void run() {
-                try {
-                    synchronized (o1) {
-                        System.out.println("synchronized o1 enter");
-                        Thread.sleep(1000);
-                        synchronized (o2) {
-                            System.out.println("synchronized o1 o2 enter");
-                        }
-                    }
-                } catch (InterruptedException e) {
-                    // TODO: handle exception
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        Thread thread2 = new Thread() {
-            public void run() {
-                try {
+        Thread thread1 = new Thread(() -> {
+            try {
+                synchronized (o1) {
+                    System.out.println("synchronized o1 enter");
+                    Thread.sleep(1000);
                     synchronized (o2) {
-                        System.out.println("synchronized o2 enter");
-                        Thread.sleep(1000);
-                        synchronized (o1) {
-                            System.out.println("synchronized o2 o1 enter");
-                        }
+                        System.out.println("synchronized o1 o2 enter");
                     }
-                } catch (InterruptedException e) {
-                    // TODO: handle exception
-                    e.printStackTrace();
                 }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        };
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                synchronized (o2) {
+                    System.out.println("synchronized o2 enter");
+                    Thread.sleep(1000);
+                    synchronized (o1) {
+                        System.out.println("synchronized o2 o1 enter");
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 
         thread1.start();
         thread2.start();
