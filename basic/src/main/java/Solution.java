feb03778f386c58546a2c31f7aa9c9883692dd1c
zhouchao3 <zhouchao3@xiaomi.com>
2019-05-21 10:06:28 +0800

    add merge k list

--- basic/src/main/java/Solution.java
+++ basic/src/main/java/Solution.java
@@ -2,20 +2,59 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(5);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(7);
-        root.left.left = new TreeNode(1);
-        root.left.right = new TreeNode(4);
-        root.right.left = new TreeNode(6);
-        root.right.right = new TreeNode(9);
+//        TreeNode root = new TreeNode(5);
+//        root.left = new TreeNode(2);
+//        root.right = new TreeNode(7);
+//        root.left.left = new TreeNode(1);
+//        root.left.right = new TreeNode(4);
+//        root.right.left = new TreeNode(6);
+//        root.right.right = new TreeNode(9);
+//
+//        ListNode head = new ListNode(1);
+//        head.next = new ListNode(2);
+////        head.next.next = new ListNode(1);
+////        head.next.next.next = new ListNode(2);
+////        head.next.next.next.next = new ListNode(1);
+//        System.out.println(new Solution().isPalindrome(head));
 
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-//        head.next.next = new ListNode(1);
-//        head.next.next.next = new ListNode(2);
-//        head.next.next.next.next = new ListNode(1);
-        System.out.println(new Solution().isPalindrome(head));
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(5);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        ListNode l3 = new ListNode(2);
+        l3.next = new ListNode(6);
+        ListNode[] lists = {l1, l2, l3};
+        mergeKLists(lists);
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists){
+        return partition(lists,0,lists.length-1);
+    }
+
+    public static ListNode partition(ListNode[] lists, int s, int e){
+        if(s==e)  return lists[s];
+        if(s<e){
+            int q=(s+e)/2;
+            ListNode l1= partition(lists,s,q);
+            ListNode l2= partition(lists,q+1,e);
+            return merge(l1,l2);
+        }else
+            return null;
+    }
+
+    //This function is from Merge Two Sorted Lists.
+    public static ListNode merge(ListNode l1,ListNode l2){
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        if(l1.val<l2.val){
+            l1.next=merge(l1.next,l2);
+            return l1;
+        }else{
+            l2.next=merge(l1,l2.next);
+            return l2;
+        }
     }
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
